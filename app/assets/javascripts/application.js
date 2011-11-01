e18e3595b6331eadd2c8a4cf63453fd3bb3edf77
Johann Chen <johannchen@gmail.com>
2011-11-01 16:49:22 -0700

    init mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,7 +7,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
-//= require jquery.validate
 //= require fullcalendar
 //= require cleditor
 //= require_tree .
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,11 +7,25 @@ class ApplicationController < ActionController::Base
  
   check_authorization
 
-  helper_method :current_user
+  helper_method :current_user, :mobile_device?
+  before_filter :prepare_for_mobile
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else 
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
   end
 end
