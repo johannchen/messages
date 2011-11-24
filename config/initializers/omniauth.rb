d40236618ce9af2d524729ef725ea24724b8f801
Johann Chen <johannchen@gmail.com>
2011-11-23 16:39:40 -0800

    0.1.0 Arnold

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,10 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  # you need a store for OpenID; (if you deploy on heroku you need Filesystem.new('./tmp') instead of Filesystem.new('/tmp'))
+
+  require 'omniauth-openid'
+  require 'openid/store/filesystem'
+  require 'omniauth-facebook'
+
+   provider :open_id, store: OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+   provider :facebook, '155210894572578', 'a92c057df6a4988b3615b379c89d8580', :scope => 'email'
+end
